00ce9d083b48ab88a8a67dba3711a791e603700c
nhtri <huutri26697@gmail.com>
2024-12-19 01:04:56 +0700

    xxx

--- app/src/main/java/com/trinh/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/trinh/basicscodelab/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.trinh.basicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,32 +28,30 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.UiMode
 import androidx.compose.ui.unit.dp
 import com.trinh.basicscodelab.ui.theme.BasicsCodelabTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BasicsCodelabTheme {
-                MyApp()
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun MyApp(
-    modifier: Modifier = Modifier
-) {
+fun MyApp(modifier: Modifier = Modifier) {
 
     val shouldShowOnboarding = rememberSaveable { mutableStateOf(true) }
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
+
+    Surface(modifier) {
         if (shouldShowOnboarding.value) {
             OnboardingScreen(onContinueClicked = { shouldShowOnboarding.value = false })
         } else {
@@ -78,6 +79,7 @@ fun OnboardingScreen(
             Text("Continue")
         }
     }
+
 }
 
 @Composable
@@ -92,21 +94,40 @@ private fun Greetings(
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val expanded = rememberSaveable { mutableStateOf(false) }
-    val extraPadding  = animateDpAsState(
-        if (expanded.value) 48.dp else 0.dp,
-        label = ""
-    )
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
 
+@Composable
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    val expanded = rememberSaveable { mutableStateOf(false) }
+
+    val extraPadding = animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding.value)
+                .padding(bottom = extraPadding.value.coerceAtLeast(0.dp))
             ) {
-                Text(text = "Hello ")
-                Text(text = name)
+                Text(text = "Hello, ")
+                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+                )
             }
             ElevatedButton(
                 onClick = { expanded.value = !expanded.value }
@@ -114,13 +135,26 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 Text(if (expanded.value) "Show less" else "Show more")
             }
         }
-
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDark"
+)
 @Composable
 fun GreetingPreview() {
     BasicsCodelabTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    BasicsCodelabTheme {
+        MyApp(Modifier.fillMaxSize())
     }
 }
